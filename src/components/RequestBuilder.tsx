31ad03c6db8dc4667fc334a4edf892c3b97f3634
vaibhavBhardwaj24 <bhardwajvaibhav2412@gmail.com>
2026-01-06 23:17:32 +0530

    ter

--- src/components/RequestBuilder.tsx
+++ src/components/RequestBuilder.tsx
@@ -73,7 +73,7 @@ export default function RequestBuilder({
           value={url}
           onChange={(e) => setUrl(e.target.value)}
           placeholder="https://api.example.com/endpoint"
-          className="flex-1 px-4 py-2 border-2 border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+          className="text-black flex-1 px-4 py-2 border-2 border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
         />
 
         <button
